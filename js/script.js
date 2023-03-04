9fde06f0534b1fcce9efd3426a6b2a41f95f2aa6
BasilGaudion <basil.gaudion@yahoo.fr>
2023-03-04 17:12:44 +0100

    Ajout fonction d'affichage à l'ecran des chiffres et operateurs + fonction effacer dernier chiffre + fonction clearAll + gestion de la classe pour la taille de la police à l'ecran en fonction du nombre de caractères + ajout de la fonction de calcul

--- js/script.js
+++ js/script.js
@@ -1,24 +1,129 @@
-// const screen = document.querySelector('.screen p');
-const buttons = document.querySelectorAll('.calc-button, .calc-button-double');
-let currentValue = "0";
+// Déclaration des variables globales
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+let result = 0;
 
-// We place an event listener at the click on each button 
-for (const button of buttons) {
-    button.addEventListener('click', event => {
-        // console.log(event.target.textContent);
-        const chiffres = event.target.textContent;
-        console.log(chiffres);
-        const screen = document.getElementById("screen");
-        screen.textContent = chiffres;
+// Initialisation of our eventListeners
+ function init() {
 
-        
-    });
+    // Récupération de chaque boutton 
+    const intButtons = document.querySelectorAll('.calc-button-number');
+    const operatorButtons = document.querySelectorAll('.calc-button-operator');
+    const clearButton = document.querySelector('.clear');
+    const eraseButton = document.querySelector('.erase');
+    const resultButton = document.querySelector('.equal');
+    const classSetter = document.querySelectorAll('button');
+    
+    // Gestion des écouteurs d'évenements
+    for (const int of intButtons) {
+        int.addEventListener('click', handleInteger);
+    }
+    for (const ope of operatorButtons) {
+        ope.addEventListener('click', handleOperator);
+    }
+    resultButton.addEventListener('click', handleResult);
+    eraseButton.addEventListener('click', handleErase);
+    clearButton.addEventListener('click', handleClear);
+
+    for (const button of classSetter) {
+        button.addEventListener('click', textSize);
+    }
+    
 }
 
-// const plus = document.getElementById("add");
+function handleInteger(event) {
+    // Récuperation des elements screen et de la valeur du bouton int cliqué
+    const screen = document.querySelector('#screen').children[0];
+    let valueInt = event.currentTarget.value;
 
+    // Condition de vidage si un operateur à été saisi alors on vide l'ecran pour afficher les chiffres cliqués
+    if (screen.textContent == operator){
+        screen.textContent = '';
+    }
 
+    // Condition pour limiter le dot à 1 maximum par screen
+    if (screen.textContent.includes('.') && valueInt == '.' ) {
+        valueInt = '';
+    }
 
-// if (chiffres.className = "add") {
-//     screen.textContent += currentValue;
-// }
+    // Ajout de la valeur du boutton int à la fin du screen
+    screen.textContent += valueInt;
+
+    // Gestion des conditions de remplissage du firstNumber ou secondNumber
+    if (firstNumber.length >= 1 && operator !== '') {
+        secondNumber = screen.textContent;
+    } else if (operator == '') {
+        firstNumber = screen.textContent;
+    }
+}
+
+function handleOperator(event) {
+    // Récuperation des elements screen et Stockage du premier chiffre
+    const screen = document.querySelector('#screen').children[0];
+    const valueOperator= event.currentTarget.value;
+
+    // On retire la classe minus pour l'operateur
+    screen.classList.remove('minus');
+
+    // Stockage de l'opérateur
+    operator = valueOperator;
+    console.log(operator);
+
+    // Affichge du contenu sur le screen
+    screen.textContent = valueOperator;
+}   
+
+function handleResult() {
+
+    // Récupération de l'élément screen
+    const screen = document.querySelector('#screen').children[0];
+
+    // Condition d'opération en fonction de l'opérateur selectionné et modification 
+    if (operator == '-') {
+        result = parseFloat(firstNumber) - parseFloat(secondNumber);
+    } else if (operator == '+') {
+        result = parseFloat(firstNumber) + parseFloat(secondNumber);
+    } else if (operator == '*') {
+        result = parseFloat(firstNumber) * parseFloat(secondNumber);
+    } else if (operator == '/') {
+        result = parseFloat(firstNumber) / parseFloat(secondNumber);
+    }
+    screen.textContent = result.toString();
+}
+
+function handleErase() {
+    // Récupération de l'élément screen
+    const screen = document.querySelector('#screen').children[0];
+
+    // Supression du dernier element de la chaine de caractère
+    screen.textContent = screen.textContent.slice(0, -1);
+}
+
+function handleClear() {
+    // Récupération de l'élément screen
+    const screen = document.querySelector('#screen').children[0];
+
+    // Vidage de toute les données enregistrées 
+    screen.textContent = '';
+    firstNumber = '';
+    operator = '';
+    secondNumber = '';
+}
+
+function textSize() {
+    // Récupération de l'élément screen
+    const screen = document.querySelector('#screen').children[0];
+
+    // Condition pour la longueur de la chaine de charactères contenue dans le screen 
+    if (screen.innerHTML.length > 15) {
+        screen.textContent = screen.textContent.slice(0, -1);
+        } else if (screen.innerHTML.length > 9) {
+                screen.classList.add('minus');
+        } else {
+                screen.classList.remove('minus');
+        }
+} 
+
+// Lancement de la fonction init au chargement complet de la page
+addEventListener('DOMContentLoaded', init);
